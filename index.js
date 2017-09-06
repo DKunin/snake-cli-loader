ff1ca6494b6aba9079d74d4ecfb6c1009e1564aa
Dmitry Kunin <dkunin@avito.ru>
2017-09-06 11:19:14 +0300

    Moved ava to dev dependecies, minor restyling

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 'use strict';
 
 var logUpdate = require('log-update');
-var chalk     = require('chalk');
+var chalk = require('chalk');
 var loopingInterval;
-var i         = 0;
-var defineColor = function(amount){
-  return amount<20?chalk.red:amount<40?chalk.yellow:chalk.green;
+var i = 0;
+var defineColor = function(amount) {
+  return amount < 20 ? chalk.red : amount < 40 ? chalk.yellow : chalk.green;
 };
 
 module.exports = {
-  start: function(){ 
-   loopingInterval = setInterval(function () {
+  start: function() {
+    loopingInterval = setInterval(function() {
       var ii = i++;
-      logUpdate( defineColor(ii)( Array(ii).join('=') ) );
-      if(i>50) { i = 0;}
+      logUpdate(defineColor(ii)(Array(ii).join('=')));
+      if (i > 50) {
+        i = 0;
+      }
     }, 25);
   },
-  stop: function(){
+  stop: function() {
     i = 0;
     clearInterval(loopingInterval);
   }
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var test        = require('ava');
+var test = require('ava');
 var snakeloader = require('./');
 
-test('loads alright',function (t) { 
-  snakeloader.start(); 
-  setTimeout(function(){
-    snakeloader.stop(); 
-    t.end();
-  }, 3000);
-  t.plan(3);
+test('loads alright', function(t) {
+    snakeloader.start();
+    setTimeout(function() {
+        snakeloader.stop();
+        t.end();
+    }, 3000);
+    t.plan(3);
 });
